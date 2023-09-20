7d08994cb352eac13e5625595c73d6d8895d825d
jperezalv23 <jperezalv@unal.edu.co>
2023-09-19 23:40:22 -0500

    Update Anfibio.java

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -2,14 +2,15 @@ package zooAnimales;
 
 public class Anfibio extends Animal{
 	private static ArrayList<Anfibio> listado;
-	int ranas;
-	int salamandras;
-	String colorPiel;
-	boolean venenoso;
+	public static int ranas;
+	public static int salamandras;
+	private String colorPiel;
+	private boolean venenoso;
 	
 	public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
 		super(nombre, edad, habitat, genero);
 		this.colorPiel = colorPiel;
+		this.venenoso = venenoso;
 		Anfibio.listado.add(this);
 	}
 	
@@ -18,11 +19,11 @@ public class Anfibio extends Animal{
 		Anfibio.listado.add(this);
 	}
 	
-	public void setListado(ArrayList<Anfibio> listado) {
+	public static void setListado(ArrayList<Anfibio> listado) {
 		this.listado = listado;
 	}
 
-	public ArrayList<Anfibio> getListado() {
+	public static ArrayList<Anfibio> getListado() {
 		return listado;
 	}
 	
@@ -34,6 +35,21 @@ public class Anfibio extends Animal{
 		this.venenoso = venenoso;
 	}
 	
+	public static void setRanas(int ranas) {
+		Anfibio.ranas = ranas;
+	}
+	
+	public static void setSalamandras(int salamandras) {
+		Anfibio.salamandras = salamandras;
+	}
+	
+	public static int getRanas() {
+		return ranas;
+	}
+	
+	public static int getSalamandras() {
+		return salamandras;
+	}
 	
 	public String getColorPiel() {
 		return colorPiel;
